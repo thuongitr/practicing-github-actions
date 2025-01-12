b89f22c57ae724b8bda727c5bfc93ef4fb845924
thelo67 <tuomo.helo@turkuamk.fi>
2025-01-12 14:21:59 +0200

    Implement 2 tests with Jest

--- Workout.test.js
+++ Workout.test.js
@@ -1,9 +1,14 @@
-import assert from "assert";
 import { Workout } from "./Workout.js";
 
-const workout = new Workout();
-workout.addToWorkout("squats");
-workout.removeFromWorkout("squats");
-assert.deepStrictEqual(workout.exercises, []);
+test("The addToWorkout function can add an exercise to the workout", () => {
+    const workout = new Workout();
+    workout.addToWorkout("squats");
+    expect(workout.exercises).toEqual(["squats"]);
+});
 
-console.log("Workout test passed");
+test("The removeFromWorkout function can remove an exercise from the workout", () => {
+    const workout = new Workout();
+    workout.addToWorkout("squats");       
+    workout.removeFromWorkout("squats");
+    expect(workout.exercises).toEqual([]);
+});
