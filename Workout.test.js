cf553b4f90416c909b28034c88d0d1d48bc9599f
thelo67 <tuomo.helo@turkuamk.fi>
2025-01-29 08:32:24 +0200

    Organize the tests with describe

--- Workout.test.js
+++ Workout.test.js
@@ -8,20 +8,32 @@ beforeEach(async () => {
 
 afterAll(async () => await closeConnection());
 
-test('createWorkout creates a workout for a username', async () => {
-    await createWorkout('Heimo Tulo');
-    const result = await db.select('username').from('workouts');
-    expect(result).toEqual([{ username: 'Heimo Tulo' }]);
+describe("createWorkout", () => {
+
+    test('create a workout for a username', async () => {
+        await createWorkout('Heimo Tulo');
+        const result = await db.select('username').from('workouts');
+        expect(result).toEqual([{ username: 'Heimo Tulo' }]);
+    });
+
 });
 
-test("addExercise adds an exercise to the workout", async () => {
-    const username = "Heimo Tulo";
-    await createWorkout(username);
-    const { id: workoutId } = await db
-        .select()
-        .from("workouts")
-        .where({ username });
-    await addExercise(workoutId, "squats");
-    const result = await db.select("exerciseName").from("workouts_exercises");
-    expect(result).toEqual([{ workoutId, exerciseName: "squats" }]);
+describe("addExercise", () => {
+
+    async function getWorkoutId(username) {
+        return await db
+            .select()
+            .from("workouts")
+            .where({ username });
+    }
+
+    test("add an exercise to the workout", async () => {
+        const username = "Heimo Tulo";
+        await createWorkout(username);
+        const { id: workoutId } = await getWorkoutId(username);
+        await addExercise(workoutId, "squats");
+        const result = await db.select("exerciseName").from("workouts_exercises");
+        expect(result).toEqual([{ workoutId, exerciseName: "squats" }]);
+    });
+
 });
